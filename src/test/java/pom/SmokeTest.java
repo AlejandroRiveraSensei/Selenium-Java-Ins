5a2595310ef9f0e70dd77c8252e464556c7ec62c
SenseiAlejandro <alejandro.rivera@senseitech.io>
2025-07-21 19:58:03 -0600

    Adding SmokeTest

---
+++ src/test/java/pom/SmokeTest.java
@@ -0,0 +1,16 @@
+package pom;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.UserData;
+
+public class SmokeTest extends BaseTest{
+
+
+    @Test
+    public void loginSuccess(){
+        login.login(UserData.getUsername("standard"), UserData.getPassword("standard"));
+        Assert.assertEquals(inventory.getTitleText(),"Products");
+    }
+
+}
